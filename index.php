955d2ff434ce50b8d1fec57352216dc29ab06ea0
TOMAS <TOMAS@TOMAS.mshome.net>
2015-05-27 18:21:45 -0300

    #1 - Commit 5

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ and open the template in the editor.
     <body>
         <?php
         //FACTURACION BASE DE DATOS
-        //HOY PIERDEN LOS BOSTEROS
+        //HOY PIERDEN LOS BOSTEROS!!
         ?>
     </body>
 </html>
